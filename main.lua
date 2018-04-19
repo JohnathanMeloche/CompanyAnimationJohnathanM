4e43e2a5ee85eb8edaf6cea217960faf745ab587
ablecry <johnathan.meloche@ocsbstudent.ca>
2018-04-19 11:05:05 -0400

    start

--- main.lua
+++ main.lua
@@ -2,3 +2,16 @@
 -- made for the app and designed by my student
 -- Johnathan M ICS20
 -- CompanyAnimation
+
+
+
+-- Hiding Status Bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Calling composer library
+local composer = require( "composer" )
+
+local pex = require "pony.com.ponywolf.pex"
+
+-- Tansitioning to the splash screen
+composer.gotoScene( "splash_screen" )
---
+++ splash_screen.lua
@@ -0,0 +1,28 @@
+-- This is the Splash Screen Scene.
+
+local Logo = display.newImageRect("Images/logo.png", 1024, 728)
+
+-- Naming Scene
+sceneName = "splash_screen"
+
+-- Creating Scene Object
+local scene = composer.newScene( sceneName )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
