24c5159829e3bb9bc37b214e077daf047fe11abf
ablecry <johnathan.meloche@ocsbstudent.ca>
2018-04-23 11:06:08 -0400

    earth

--- splash_screen.lua
+++ splash_screen.lua
@@ -15,27 +15,79 @@ local scene = composer.newScene( sceneName )
 ------------------------------------------------------
 -- The function called when the screen doesn't exist
 function scene:create( event )
+
+	-- Declares Local Image Variables
+
+	local earthRotate
+	local riseSpeed = 100
+	local logoRise
+
+
+
+
+
 	-- Displays Part of the Company logo to be used for animation and sets properties
-	local logoSword = display.newImageRect("Images/splashScreenVariations/logosword.png", 1024, 728)
+	local logoSword = display.newImageRect("Images/splashScreenVariations/logosword.png", 1400, 1000)
 	logoSword.x = 800
 	logoSword.y = 700
 
 	-- Displays Part of the Company logo to be used for animation and sets properties
-	local logoSwordBroken = display.newImageRect("Images/splashScreenVariations/logoswordbroken.png", 1024, 728)
-	logoSwordBroken.x = 500
-	logoSwordBroken.y = 500
+	local logoSwordBroken = display.newImageRect("Images/splashScreenVariations/logoswordbroken.png", 1400, 1000)
+	logoSwordBroken.x = 155
+	logoSwordBroken.y = 650
+	logoSwordBroken.isVisible = false
 
 	-- Displays Part of the Company logo to be used for animation and sets properties
-	local logoEarth = display.newImageRect("Images/splashScreenVariations/logoEarth.png", 1024, 728)
-	logoEarth.x = 800
+	local logoEarth = display.newImageRect("Images/splashScreenVariations/logoEarth.png", 500, 500)
+	logoEarth.x = 1100
 	logoEarth.y = 300
 
 	-- Displays Part of the Company logo to be used for animation and sets properties
-	local logoF = display.newImageRect("Images/splashScreenVariations/logoF.png", 1024, 728)
-	logoF.x = 400
-	logoF.y = 500
+	local logoF = display.newImageRect("Images/splashScreenVariations/logoF.png", 1400, 1000)
+	logoF.x = 150
+	logoF.y = 2000
+
+	-- Y must be 600
+
+	-- Displays Part of the Company logo to be used for animation and sets properties
+	local logoTitle = display.newImageRect("Images/splashScreenVariations/logoTitle.png", 1800, 200)
+	logoTitle.x = 500
+	logoTitle.y = 1300
+
+
+
+	local function logoRiseUp()
+		logoRise = logoF.y - riseSpeed
+	end
+
+	local function rotateEarth()
+		earthRotate = logoEarth:rotate(.7)
+		timer.performWithDelay(300, earthRotate)
+		logoRiseUp
+	end
+
+
+	Runtime:addEventListener("enterFrame", rotateEarth)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
+
+
+scene:addEventListener( "create", scene )
+
+
 
 -------------------------------------------------------
 
